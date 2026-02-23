c46d7fc59ca071285ad8e84f2435e313f8da00af
Ashwanth Reddy <ashreddy06b@gmail.com>
2026-02-23 15:42:13 +0530

    table manipulation with jpa

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,11 +1,53 @@
 package com.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "bookingreview")
 public class Review {
 
   @Id // Primary key
-  Long id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String content;
+
+  private Double rating;
+
+  @Column(nullable = false)
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @Column(nullable = false)
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @Override
+  public String toString() {
+    return "Review: " + this.content + " " + this.rating + " ";
+  }
 }
---
+++ src/main/java/com/example/uberreviewservice/services/ReviewService.java
@@ -0,0 +1,39 @@
+package com.example.uberreviewservice.services;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import com.example.uberreviewservice.models.Review;
+import com.example.uberreviewservice.repositories.ReviewRepository;
+
+@Service
+public class ReviewService implements CommandLineRunner {
+
+  private ReviewRepository reviewRepository;
+
+  public ReviewService(ReviewRepository reviewRepository) {
+    this.reviewRepository = reviewRepository;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    /*
+     * System.out.println("********");
+     * Review r = Review
+     * .builder()
+     * .content("Amazing ride")
+     * .rating(5.0)
+     * .build();
+     * System.out.println(r);
+     * reviewRepository.save(r); // sql query here comes
+     * List<Review> reviews = reviewRepository.findAll();
+     * 
+     * for (Review review : reviews) {
+     * System.out.println(review.getContent());
+     * }
+     */
+    reviewRepository.deleteById(3L);
+  }
+}
