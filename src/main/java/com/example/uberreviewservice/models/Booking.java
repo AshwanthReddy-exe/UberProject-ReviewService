bc8179d35c3acdc593f4b492e4e419243b7fc149
Ashwanth Reddy <ashreddy06b@gmail.com>
2026-02-26 00:31:54 +0530

    added booking cascaded with review

---
+++ src/main/java/com/example/uberreviewservice/models/Booking.java
@@ -0,0 +1,36 @@
+package com.example.uberreviewservice.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel {
+
+  @OneToOne(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+  private Review review; // OneToOne rel with reviews
+
+  @Enumerated(value = EnumType.STRING)
+  private BookingStatus bookingStatus;
+
+  private LocalDateTime startTime;
+
+  private LocalDateTime endTime;
+
+  private Long totalDistance;
+
+}
--- src/main/java/com/example/uberreviewservice/services/ReviewService.java
+++ src/main/java/com/example/uberreviewservice/services/ReviewService.java
@@ -1,33 +1,47 @@
 package com.example.uberreviewservice.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import com.example.uberreviewservice.models.Booking;
 import com.example.uberreviewservice.models.Review;
+import com.example.uberreviewservice.repositories.BookingRepository;
 import com.example.uberreviewservice.repositories.ReviewRepository;
 
 @Service
 public class ReviewService implements CommandLineRunner {
 
   private ReviewRepository reviewRepository;
+  private BookingRepository bookingRepository;
 
-  public ReviewService(ReviewRepository reviewRepository) {
+  public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository) {
     this.reviewRepository = reviewRepository;
+    this.bookingRepository = bookingRepository;
   }
 
   @Override
   public void run(String... args) throws Exception {
 
     System.out.println("********");
+
     Review r = Review
         .builder()
         .content("Amazing ride")
         .rating(5.0)
         .build();
+
+    Booking b = Booking.builder()
+        .endTime(LocalDateTime.now())
+        .review(r)
+        .build();
+
     System.out.println(r);
-    reviewRepository.save(r); // sql query here comes
+    // reviewRepository.save(r); // sql query here comes
+    bookingRepository.save(b);
     System.out.println(r.getId());
     List<Review> reviews = reviewRepository.findAll();
 
@@ -35,6 +49,11 @@ public class ReviewService implements CommandLineRunner {
       System.out.println(review.getContent());
     }
 
-    reviewRepository.deleteById(3L);
+    Optional<Booking> b2 = bookingRepository.findById(5L);
+    System.out.println(b2);
+    if (b2.isPresent()) {
+      bookingRepository.delete(b2.get());
+    }
+    // reviewRepository.deleteById(3L);
   }
 }
